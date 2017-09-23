08cbbd078fb07755ce00c9edab1583acd076d44b
Jesse <Jesse@dyn-209-2-208-110.dyn.columbia.edu>
2017-09-23 17:09:54 -0400

    toPy3

--- Search.py
+++ Search.py
@@ -1,18 +1,35 @@
 import requests
 import json
+import sys
 
 
 
 SearchAPI = "https://www.googleapis.com/customsearch/v1"
-CustomEnginekey = '014170202143592210537:4zb34sjofuu'
-JSONkey = 'AIzaSyBz-iFhhFx_sQSBMxKBMh9d5ZjD2nyQtLw'
-
-query = "Columbia"
 
 
-def googleQuery(query):
-	payload = {'cx': CustomEnginekey , 'key': JSONkey ,'q' : query}
+def googleQuery(CSEKey, JsonAPIKey, precsion, query):
+	payload = {'cx': CSEKey , 'key': JsonAPIKey ,'q' : query}
 	r = requests.get(SearchAPI, params=payload)
 	return json.loads(r.text)
 
-print googleQuery(query)
+
+def main():
+
+    JsonAPIKey = sys.argv[1]
+    CSEKey = sys.argv[2]
+    precision = sys.argv[3]
+    query = sys.argv[4]
+    print(googleQuery(CSEKey, JsonAPIKey, precision, query))
+
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print ("\n")
+        exit()	
+
+
+
