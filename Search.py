95a5e15f8fb64f3d72accbe8158c9b7f2131b405
Lanking <lanking520@live.com>
2017-09-23 16:49:12 -0400

    add Basic Search

--- Search.py
+++ Search.py
@@ -1,2 +1,18 @@
 import requests
+import json
 
+
+
+SearchAPI = "https://www.googleapis.com/customsearch/v1"
+CustomEnginekey = '014170202143592210537:4zb34sjofuu'
+JSONkey = 'AIzaSyBz-iFhhFx_sQSBMxKBMh9d5ZjD2nyQtLw'
+
+query = "Columbia"
+
+
+def googleQuery(query):
+	payload = {'cx': CustomEnginekey , 'key': JSONkey ,'q' : query}
+	r = requests.get(SearchAPI, params=payload)
+	return json.loads(r.text)
+
+print googleQuery(query)
