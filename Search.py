38b63e63c71fcefa9f2f64c7f5ac8791c1ec49d1
Lanking <lanking520@live.com>
2017-09-23 17:45:45 -0400

    Add data clean library

--- Search.py
+++ Search.py
@@ -4,14 +4,21 @@ import sys
 import re
 
 SearchAPI = "https://www.googleapis.com/customsearch/v1"
+libraryPath = "./proj1-stop.txt"
 
-def dataClean(query):
-    query = re.sub('[^a-zA-Z0-9]', ' ', query)
-    return query
+class DataClean:
 
-def stopWordRemoval(text, library):
-     text = [w for w in text if not w in library and not w.replace('.', '', 1).isdigit()]
-     return text
+    def __init__(self, path):
+        f = open(path)
+        self.library = f.readlines()
+
+    def clean(self, query):
+        query = re.sub('[^a-zA-Z0-9]', ' ', query)
+        return query
+
+    def stopWordRemoval(self, text):
+        text = [w for w in text if not w in self.library and not w.replace('.', '', 1).isdigit()]
+        return text
 
 
 def googleQuery(CSEKey, JsonAPIKey, precsion, query):
@@ -27,6 +34,7 @@ def main():
     CSEKey = sys.argv[2]
     precision = float(sys.argv[3])
     query = ' '.join(sys.argv[4:])
+    DC = DataClean(libraryPath)
     # googleQuery(CSEKey, JsonAPIKey, precision, query)
     sys.stdout.write(query)
 
