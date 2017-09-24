96e5645b8615fda020dc27c54b56625215bc613f
Jesse Liu <Jesse@dyn-209-2-208-253.dyn.columbia.edu>
2017-09-23 20:23:33 -0400

    pos and neg and summary

--- Search.py
+++ Search.py
@@ -2,17 +2,19 @@ import requests
 import json
 import sys
 import re
+# import numpy as np
 
 SearchAPI = "https://www.googleapis.com/customsearch/v1"
 libraryPath = "./proj1-stop.txt"
 
-class DataClean:
 
+class DataClean:
     def __init__(self, path):
         f = open(path)
         self.library = f.read().splitlines()
 
     def clean(self, query):
+        # Half-piece
         query = re.sub('[^a-zA-Z0-9]', ' ', query)
         return query
 
@@ -22,9 +24,22 @@ class DataClean:
 
 
 def googleQuery(CSEKey, JsonAPIKey, precsion, query):
-	payload = {'cx': CSEKey , 'key': JsonAPIKey ,'q' : query}
-	r = requests.get(SearchAPI, params=payload)
-	return json.loads(r.text)
+    payload = {'cx': CSEKey , 'key': JsonAPIKey ,'q' : query}
+    r = requests.get(SearchAPI, params=payload)
+    return json.loads(r.text)
+
+def printResult(item):
+
+    sys.stdout.write('Title: '+item["title"]+'\n')    
+    sys.stdout.write('Link: '+item["link"]+'\n')
+    sys.stdout.write('Summary: '+item["snippet"]+'\n')
+    ans = input('Relevant(Y/N)?')
+    if ans=='y' or ans=='Y':
+        return True
+    else:
+        return False
+
+
 
 def main():
     if len(sys.argv) < 4:
@@ -35,10 +50,34 @@ def main():
     precision = float(sys.argv[3])
     query = ' '.join(sys.argv[4:])
     DC = DataClean(libraryPath)
-    # googleQuery(CSEKey, JsonAPIKey, precision, query)
-    sys.stdout.write(query)
+    DC.clean(query)
+    
 
+    alpha = 1
+    beta = 1.0
+    gamma = 0.5
 
+    result = googleQuery(CSEKey, JsonAPIKey, precision, query)
+    result_items = result["items"];
+    summary_set = []
+    pos = []
+    neg = []
+    if len(result_items) ==0:
+        print("No result found")
+    else:
+        for item in result_items:
+            tmp = []
+            tmp = item["snippet"].split(' ')
+            for string in tmp:
+                summary_set.append(string)            
+            if printResult(item):
+                pos.append(item)
+            else:
+                neg.append(item)
+
+    
+
+    
 
 
 if __name__ == "__main__":
