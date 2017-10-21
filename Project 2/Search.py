46e57001fefb0fce068c97434dc93d5c67d07d0f
Lanking <lanking520@live.com>
2017-10-21 15:03:33 -0400

    add Contents

--- Project 2/Search.py
+++ Project 2/Search.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import json
 import sys
 import re
@@ -12,6 +13,12 @@ def googleQuery(CSEKey, JsonAPIKey, query):
     r = requests.get(SearchAPI, params=payload)
     return json.loads(r.text)
 
+def textExtractor(URL):
+	html = open(URL).read()
+	soup = BeautifulSoup(html)
+	[s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+	visible_text = soup.getText()
+
 def main():
     if len(sys.argv) < 7:
         sys.stdout.write('Usage : python3 '+ sys.argv[0] +' <google api key> <google engine id> <r> <t> <q> <k>\n')
@@ -22,7 +29,9 @@ def main():
     ECT = sys.argv[4]					# extraction confidence threshold
     query = sys.argv[5]
     k = sys.argv[6]
-    
+    # Initialization
+    ExtractedTuple = {}
+    SeenURL = {}
     sys.stdout.write("Hello World!")
     
 
