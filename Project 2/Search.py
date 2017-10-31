d4fa49326077d7dd4855b9cdde244db4609ff66c
Lanking <lanking520@live.com>
2017-10-30 23:59:22 -0400

    buget work solution 1

--- Project 2/Search.py
+++ Project 2/Search.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from NLPCore import NLPCoreClient
+from collections import defaultdict
 import urllib
 import json
 import sys
@@ -16,7 +17,7 @@ def googleQuery(CSEKey, JsonAPIKey, query):
     r = requests.get(SearchAPI, params=payload)
     return json.loads(r.text)
 
-def textExtractor(URL):
+def textExtractor(URL, min = 10, max = 30):
     # Default timeout 10 seconds
     r = requests.get(URL, timeout = 10)
     ok = False
@@ -25,23 +26,49 @@ def textExtractor(URL):
         soup = BeautifulSoup(r.text, "html.parser")
         [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
         visible_text = soup.getText()
-        import re
-        splitted = re.split(r'[^a-zA-Z0-9 ,\'&\-\(\)]+', visible_text)
-        cleaned = []
-        for item in splitted:
-            length = len(item.split())
-            if length > 10 and length < 30:
-                cleaned.append(item)
+        # import re
+        # splitted = re.split(r'[^a-zA-Z0-9 ,\'&\-\(\)]+', visible_text)
+        cleaned = visible_text.split('\n')
+        # for item in splitted:
+        #     length = len(item.split())
+        #     if length > min and length < max:
+        #         cleaned.append(item)
         ok = True
 
     return ok, cleaned
 
-def NLP(text):
+
+def Round1(text):
+    result = []
+    properties = {
+        "annotators": "tokenize,ssplit,pos,lemma,ner",
+        "parse.model": "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz",
+        "ner.useSUTime": "0"
+    }
+    relation_name = 'Work_For'
+    ne1_type = "ORGANIZATION"
+    ne2_type = "PERSON"
+    doc = client.annotate(text = text, properties = properties)
+    print("Number of sentences in round 1",len(doc.sentences))
+    for sentence in doc.sentences:
+        sb = []
+        typechecker = defaultdict(int)
+        for token in sentence.tokens:
+            # print(token.ner)
+            typechecker[token.ner] += 1
+            sb.append(token.word)
+        if typechecker[ne1_type] > 0 and typechecker[ne2_type] > 0 and len(sb) < 50:
+            line = ' '.join(sb)
+            result.append(line)
+    return result
+
+
+
+def Round2(text):
     # Path
     # for item in text:
     # 	if type(item) !='str':
     # 		print("Crack on items")
-    
     properties = {
         "annotators": "tokenize,ssplit,pos,lemma,ner,parse,relation",
         "parse.model": "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz",
@@ -50,6 +77,7 @@ def NLP(text):
     relation_name = 'Work_For'
     ne1_type = "ORGANIZATION"
     ne2_type = "PEOPLE"
+    threshold = 0.35
     doc = client.annotate(text = text, properties = properties)
     for item in doc.sentences:
         r = item.relations
@@ -60,21 +88,31 @@ def NLP(text):
                 if max_val < float(value):
                     max_val = float(value)
                     max_rel_desc = key
-            if max_rel_desc == relation_name:
+            if max_rel_desc == relation_name and max_val >= threshold:
                 if [ne1_type, ne2_type] == sorted([relation.entities[0].type, relation.entities[1].type]):
                     print(relation)
 
 def test():
     ok, extracted = textExtractor("https://en.wikipedia.org/wiki/Bill_Gates")
     if ok:
-        print("Prepare to Processing",len(extracted))
-        print(extracted)
+        print("Prepare to Processing Round1",len(extracted))
         length = 0
-        while length + 10 < len(extracted):
-            NLP(extracted[length : length + 10])
-            length +=10
+        chunck_size = 1000
+        text = []
+        while length + chunck_size < len(extracted):
+            text += Round1(extracted[length:length + chunck_size])
+            length += chunck_size
             print("Finished", length,"/",len(extracted))
-        NLP(extracted[length : len(extracted)])
+
+        text += Round1(extracted[length : len(extracted)])
+
+        print("Prepare to Processing Round2",len(text))
+        length = 0
+        while length + chunck_size < len(text):
+            Round2(text[length : length + chunck_size])
+            length += chunck_size
+            print("Finished", length,"/",len(text))
+        Round2(text[length : len(text)])
         # print(extracted)
     else:
         print("Error Happened during the fetching and cleaning steps")
